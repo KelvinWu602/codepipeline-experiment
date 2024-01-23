304c9d0528b8a1376639ebdc30d1625898d5ab5d
KelvinWu <kelvinwu@beNovelty.com>
2024-01-23 10:45:35 +0800

    only echo hello world first

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -6,13 +6,11 @@ export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id,props);
 
-        new CodePipeline(this, 'Pipeline', {
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'WorkshopPipeline',
             synth: new ShellStep('Synth', {
                 input: CodePipelineSource.gitHub('KelvinWu602/codepipeline-experiment', 'main'),
-                commands: ['echo "Hello world!!!!!!!"',
-                           'npm ci',
-                           'npm run build',
-                           'npx cdk synth']
+                commands: ['echo "Hello world!!!!!!!"']
             })
         })
     }
